17bcf28e2ba6a445d3b976c47afb91e98742a475
jyothik05 <jyothikaswamykore@gmail.com>
2025-03-19 23:32:21 +0530

    python

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+'''Python Data Types:
+
+Built-in Data Types:
+
+
+In programming, data type is an important concept.
+Variables can store data of different types, and different types can do different things.
+
+Python has the following data types built-in by default, in these categories:
+
+Text Type     :	str
+Numeric Types :	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type  :	dict
+Set Types     :	set, frozenset
+
+Getting the Data Type:
+
+You can get the data type of any object by using the type() function:
+
+Example:
+
+    x = 5
+    print(type(x))
+
+datatypes program is there in the folder 'program.py' file name is 'datatypes.py'  you can check out there...!!'''
